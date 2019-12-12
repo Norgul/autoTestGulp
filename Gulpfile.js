fff0986f09fcf75d7d8feb848057b6b147742b61
Norgul <Norgul@users.noreply.github.com>
2019-12-12 14:03:10 +0100

    Update Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,26 +1,53 @@
-var gulp = require('gulp');
-var phpspec = require('gulp-phpspec');
-var run = require('gulp-run');
-var notify = require('gulp-notify');
+/**
+ * If you don't like the verbosity level of notifications you can remove the [, verbose: 'v'] at the end of line 35.
+ *
+ * And if you have more than 1 file in spec directory is better to create phpspec.yml in root project directory,
+ * it doesn't matter if phpspec.yml will be empty or properly configured,
+ * and comment line 34 and uncomment line 33.
+ *
+ * For this file to work properly you need to install:
+ * brew install node
+ * npm install --global gulp-cli
+ * npm install gulp gulp-phpspec gulp-notify
+ *
+ * Just in case my setup:
+ * OS: macOS Catalina
+ * node -v: v13.2.0
+ * npm -v: 6.13.1,
+ * npx -v: 6.13.1
+ * gulp -v: CLI version: 2.2.0 Local version: 4.0.2
+ *
+ * package.json:
+ * "devDependencies": {
+ *   "gulp": "^4.0.2",
+ *   "gulp-notify": "^3.2.0",
+ *   "gulp-phpspec": "^0.5.5",
+ * }
+ */
 
-gulp.task('test', function(done){
+const gulp = require('gulp');
+const phpspec = require('gulp-phpspec');
+const notify = require('gulp-notify');
+
+gulp.task('test', function (done) {
+    // gulp.src('phpspec.yml')
     gulp.src('spec/**/*.php')
-        .pipe(run('clear').exec())
-        .pipe(phpspec('', {notify:true}))
-        .on('error', notify.onError({
-            'title': 'Crap',
-            'message': 'Your tests failed miserably!',
-            // 'icon': __dirname + '/fail.png',
-        }))
-        .pipe(notify({
-            'title': 'Success',
-            'message': 'All tests have returned green!'
-        }));
+        .pipe(phpspec('', {notify: true, clear: true, noInteraction: true, debug: true, verbose: 'v'}))
+        .on('error', notify.onError(testNotification('fail')))
+        .pipe(notify(testNotification('pass')));
     done();
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch(['spec/**/*.php', 'src/**/*.php'], gulp.series(['test']));
 });
 
-gulp.task('default', gulp.series(['test','watch']));
+gulp.task('default', gulp.series(['test', 'watch']));
+
+function testNotification(status) {
+    return {
+        title: (status === 'pass') ? 'Tests Passed' : 'Tests Failed',
+        message: (status === 'pass') ? '\n\nAll tests have passed!\n\n' : '\n\nOne or more tests failed...\n\n',
+        icon: __dirname + '/node_modules/gulp-phpspec/assets/test-' + status + '.png'
+    }
+}
